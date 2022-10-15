7caaf10790ff6aebd27f8ac8b565e43ab3bf50c8
WargoodSetru <maks_mazurin@inbox.ru>
2022-10-16 02:55:32 +0300

    index.php lab1

---
+++ lab1/calculation.php
@@ -0,0 +1,8 @@
+Длина средней линии трапеции равна: 
+<?php 
+$result = ($_GET["AB_foundation"] + $_GET["CD_foundation"]) / 2;// Длина средней линии трапеции
+echo $result
+?> 
+
+
+
---
+++ lab1/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+ <head>
+    <meta charset="UTF-8">
+  <title> lab1 </title>
+ </head>
+ <body>
+
+     <form action="calculation.php" method="get">
+ Вариант 13 <p>Вычислить длину средней линии трапеции<p> 
+     <h1>Введите следующие значения:</h1>
+     Основание AB: <input type="text" name="AB_foundation"><br>
+     Основание CD: <input type="text" name="CD_foundation"><br>
+     <input type="submit">
+     </form>
+     
+ </body>
+</html>
